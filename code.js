acd38b0fc36207e5b6751673c1ed8712fca58685
Dario Cruz <rio2012@gmail.com>
2022-08-29 13:43:58 -0400

    Add eventlisteners for all calc buttons

--- code.js
+++ code.js
@@ -15,6 +15,7 @@ const sevenButton = document.getElementById("seven")
 const eightButton = document.getElementById("eight")
 const nineButton = document.getElementById("nine")
 const zeroButton = document.getElementById("zero")
+// compuView.innerHTML = " "
 
 // Number window on calc 
 const compuView = document.getElementById("compuview")
@@ -22,5 +23,24 @@ const compuView = document.getElementById("compuview")
 function addText(text) {
     compuView.innerHTML += text;
 }
+function clearText() {
+    compuView.innerHTML = "";
+}
 
-zeroButton.addEventListener("click", addText("0"));
+zeroButton.addEventListener("click", () => addText("0"));
+oneButton.addEventListener("click", () => addText("1"));
+twoButton.addEventListener("click", () => addText("2"));
+threeButton.addEventListener("click", () => addText("3"));
+fourButton.addEventListener("click", () => addText("4"));
+fiveButton.addEventListener("click", () => addText("5"));
+sixButton.addEventListener("click", () => addText("6"));
+sevenButton.addEventListener("click", () => addText("7"));
+eightButton.addEventListener("click", () => addText("8"));
+nineButton.addEventListener("click", () => addText("9"));
+
+clearButton.addEventListener("click", () => clearText());
+
+multiplyButton.addEventListener("click", () => addText("*"));
+divideButton.addEventListener("click", () => addText("/"));
+additionButton.addEventListener("click", () => addText("+"));
+subtractButton.addEventListener("click", () => addText("-"));
